26d0abd269e6d1f3d8e5fad869b6e6407c865562
obwansan <kob123@hotmail.co.uk>
2018-03-08 15:23:47 +0000

    HTML form structure and first validation conditional

---
+++ account.php
@@ -0,0 +1,13 @@
+<?php
+
+$errorMessage = '';
+
+if ( !is_string($_POST['username']) || !is_string($_POST['password'])) {
+    $errorMessage += 'Invalid input: you are not logged in';
+}
+
+
+
+if () {
+
+}
---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login Page</title>
+</head>
+<body>
+
+    <form method="post" action="account.php">
+        <label>Username:
+            <input type="text" name="username">
+        </label>
+        <label>Password:
+            <input type="text" name="password">
+        </label>
+        <input type="submit">
+    </form>
+
+</body>
+</html>
