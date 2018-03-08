27dd171fb0967230f184fbe4d16e44fd6d432315
obwansan <kob123@hotmail.co.uk>
2018-03-08 15:39:53 +0000

    Completed validation - is string and input length

--- account.php
+++ account.php
@@ -1,6 +1,9 @@
 <?php
 
 $errorMessage = '';
+$successMessage = 'You are logged in'
+$usernameLength = strlen($_POST['username']);
+$passwordLength = strlen($_POST['password']);
 
 if ( !is_string($_POST['username'])) {
     $errorMessage += 'Invalid username: you are not logged in';
@@ -10,11 +13,21 @@ if ( !is_string($_POST['password'])) {
     $errorMessage += 'Invalid password: you are not logged in';
     $errorMessage += '<br>';
 }
-
-
-$usernameLength = strlen($_POST['username']);
-$passwordLength = strlen($_POST['password']);
-
 if ($usernameLength < 5 || $usernameLength > 30) {
-
+    $errorMessage += 'Invalid username: you are not logged in';
+    $errorMessage += '<br>';
 }
+
+if ($passwordLength < 5 || $passwordLength > 30) {
+    $errorMessage += 'Invalid password: you are not logged in';
+    $errorMessage += '<br>';
+}
+
+if (empty($errorMessage)) {
+    echo $successMessage;
+} else {
+    echo $errorMessage;
+}
+
+
+
