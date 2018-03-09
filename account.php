81a4c92646580552e64f02380d83a3ed77948e04
obwansan <kob123@hotmail.co.uk>
2018-03-09 22:41:54 +0000

    Added strlen() to get UN and PW length

--- account.php
+++ account.php
@@ -4,20 +4,30 @@ session_start();
 var_dump($_SESSION);
 
 $username = $_POST['username'];
+var_dump($_POST['username']);
+
 $password = $_POST['password'];
-$correctUsername = 'username';
-$correctPassword = 'password';
-$errorMessage = '';
-$usernameLength = strlen($_POST['username']);
-$passwordLength = strlen($_POST['password']);
+var_dump($_POST['password']);
 
-function validateInput($username, $password) {
+$correctUsername = 'obwansan';
+$correctPassword = 'turnip204';
 
-    if ($username != 'username' || !is_string($username) || $usernameLength < 5 || $usernameLength > 30) {
-        $errorMessage += 'Invalid username' . '<br>';
+$usernameLength = strlen($username);
+var_dump($usernameLength);
+
+$passwordLength = strlen($password);
+var_dump($passwordLength);
+
+
+function validateInput($username, $usernameLength, $password, $passwordLength) {
+
+    $errorMessage = '';
+
+    if ($username != 'obwansan' || !is_string($username) || $usernameLength < 5 || $usernameLength > 30) {
+        $errorMessage = 'Invalid username' . '<br>';
     }
     if ($password != 'password' || !is_string($password) || $passwordLength < 5 || $passwordLength > 30) {
-        $errorMessage += 'Invalid password' . '<br>';
+        $errorMessage .= 'Invalid password' . '<br>';
     }
     if ($errorMessage == '') {
         return 'Your input is valid';
@@ -27,12 +37,18 @@ function validateInput($username, $password) {
 }
 
 function checkCredentials($username, $correctUsername, $password, $correctPassword) {
-    if ($username == $correctUsername && $password == $correctPassword || $_SESSION['loggedIn'] = true) {
+
+    if ($username == $correctUsername && $password == $correctPassword) {
         $_SESSION['loggedIn'] = true;
+        header('Location: account.php');
         return 'Logged in';
     } else {
         $_SESSION['loggedIn'] = false;
-        header('Location: index.php');
+        var_dump($username);
+        var_dump($correctUsername);
+//        var_dump($password);
+//        var_dump($correctPassword);
+//        header('Location: index.php');
         return "Not logged in";
     }
 }
