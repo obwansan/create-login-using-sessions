e5179d67b1a07517f25b3b14d928ea829b8ad9cb
obwansan <kob123@hotmail.co.uk>
2018-03-10 08:03:58 +0000

    Got it to work with just checkCredentials()

--- account.php
+++ account.php
@@ -4,10 +4,10 @@ session_start();
 var_dump($_SESSION);
 
 $username = $_POST['username'];
-var_dump($_POST['username']);
+var_dump($username);
 
 $password = $_POST['password'];
-var_dump($_POST['password']);
+var_dump($password);
 
 $correctUsername = 'obwansan';
 $correctPassword = 'turnip204';
@@ -18,16 +18,16 @@ var_dump($usernameLength);
 $passwordLength = strlen($password);
 var_dump($passwordLength);
 
-
-function validateInput($username, $usernameLength, $password, $passwordLength) {
+/*
+function validateInput($username,  $password ) {
 
     $errorMessage = '';
 
-    if ($username != 'obwansan' || !is_string($username) || $usernameLength < 5 || $usernameLength > 30) {
-        $errorMessage = 'Invalid username' . '<br>';
+    if ($username != 'obwansan' ) {
+        $errorMessage = '<p>Invalid username</p>';
     }
-    if ($password != 'password' || !is_string($password) || $passwordLength < 5 || $passwordLength > 30) {
-        $errorMessage .= 'Invalid password' . '<br>';
+    if ($password != 'turnip204' ) {
+        $errorMessage .= '<p>Invalid password</p>';
     }
     if ($errorMessage == '') {
         return 'Your input is valid';
@@ -35,27 +35,28 @@ function validateInput($username, $usernameLength, $password, $passwordLength) {
         return $errorMessage;
     }
 }
+*/
 
 function checkCredentials($username, $correctUsername, $password, $correctPassword) {
 
     if ($username == $correctUsername && $password == $correctPassword) {
         $_SESSION['loggedIn'] = true;
-        header('Location: account.php');
+//        header('Location: account.php');
         return 'Logged in';
     } else {
         $_SESSION['loggedIn'] = false;
-        var_dump($username);
-        var_dump($correctUsername);
-//        var_dump($password);
-//        var_dump($correctPassword);
 //        header('Location: index.php');
-        return "Not logged in";
+        return 'Not logged in';
     }
 }
 
-echo validateInput($username, $password);
+//echo validateInput($username, $password);
 echo checkCredentials($username, $correctUsername, $password, $correctPassword);
 
-
+/*
+ * || !is_string($username) || $usernameLength < 5 || $usernameLength > 30
+ * || !is_string($password) || $passwordLength < 5 || $passwordLength > 30
+ *
+ * */
 
 
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 session_start();
-var_dump($_SESSION);
+//var_dump($_SESSION);
 
 if ($_SESSION['loggedIn']) {
     header('Location: account.php');
